941107f294d8aa87f8ff238da56be439e54cd208
Matt Martz <matt.martz@gmail.com>
2025-11-30 09:35:09 -0800

    Initial commit

---
+++ test/r2r.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as R2R from '../lib/r2r-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/r2r-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new R2R.R2RStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
